73ac18f769df9d9361e7fcc2b8ff00c5d2ec49fd
MacioO-69 <63945390+MacioO-69@users.noreply.github.com>
2023-04-12 13:50:00 +0200

    Add files via upload

---
+++ add.php
@@ -0,0 +1,16 @@
+<?php
+    $marka = $_POST["marka"];
+    $model = $_POST["model"];
+    $rocznik = $_POST["rocznik"];
+    $przebieg = $_POST["przebieg"];
+    $moc = $_POST["moc"];
+    $cena = $_POST["cena"];
+    $rodzajpaliwa = $_POST["rodzajpaliwa"];
+
+
+    $conn = mysqli_connect("localhost","root","","xdxd");
+
+    $sql = "INSERT INTO samochody(marka,model,rocznik,moc,przebieg,cena,rodzajpaliwa) values ('$marka','$model',$rocznik,$moc,$przebieg,$cena,$rodzajpaliwa)";
+
+    mysqli_query($conn,$sql);
+    ?>
