67ec3dc1a9cb87bddd2a491afa5e172f67396d62
MacioO-69 <63945390+MacioO-69@users.noreply.github.com>
2023-04-12 13:41:47 +0200

    Add files via upload

---
+++ ogloszenie.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form action="wyszukaj.php" method="POST">
+        <table>
+        <tr><td>Marka</td><td><input type="text" name = "marka" placeholder="Marka"></td></tr>
+        <tr><td>Model</td><td><input type="text" name = "model" placeholder="Model"></td></tr>
+        <tr><td>Rocznik</td><td><input type="number" max='2023' min='1900' name = "yearod" placeholder="Od"><input type="number" max='2023' min='1900' name = "yeardo" placeholder="Do"></td></tr>
+        <tr><td>Przebieg</td><td><input type="number"  name = "przebiegod" min='0' max='9999999' placeholder="Od "><input type="number"  name = "przebiegod" min='0' max='9999999' placeholder="Do"></td></tr>
+        <tr><td>Moc</td><td><input type="number" name = "mocod" placeholder="Od " min='0' max='9999'><input type="number" name = "mocdo" placeholder="Do " min='0' max='9999'></td></tr>
+        <tr><td>Cena</td><td><input type="number" name = "cenaod" placeholder="Od"  min='0' max='9999999'><input type="number" name = "cenado" placeholder="Do"  min='0' max='9999999'></td></tr>
+        <tr><td>Rodzaj paliwa</td><td><select name = "rodzaj paliwa" placeholder="Wybierz ">
+            <option value="Elektryk">Elektryczny</option>
+            <option value="Diesel">Diesel</option>
+            <option value="Benzyna">Benzyna</option>
+            <option value="Gaz">Gaz</option>
+            <option value="Wodór">Wodór</option>
+        </select></td></tr>
+</table>
+<input type="submit" value="Wyślij">
+    </form>
+</body>
+</html>
